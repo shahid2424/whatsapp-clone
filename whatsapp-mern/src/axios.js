d5502b650bbcc241de7f5a7359f6e65ca604715a
shahid2424 <mohammedshahid2431@gmail.com>
2023-04-09 12:15:47 +0530

    url update

--- whatsapp-mern/src/axios.js
+++ whatsapp-mern/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:9000",
+    baseURL: "https://whatsapp-backend-93jj.onrender.com",
 });
 
 export default instance;
